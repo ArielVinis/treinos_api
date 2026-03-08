a5d8d3dbcd282a9b6ea6597fe3d45c4f2498af28
France Dev <france.dev@outlook.com>
2026-03-08 16:54:24 -0300

    add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT), host: "0.0.0.0" });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
